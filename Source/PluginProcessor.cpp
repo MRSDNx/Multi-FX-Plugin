3f108638e047de1ef372cb486db5f2faf31e15ce
Kingbnjmn <business@kingbnjmn.com>
2025-05-11 09:53:20 -0500

    Added sorting and processing of DSP pointers

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -134,7 +134,7 @@ void Project13AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, ju
     juce::ScopedNoDenormals noDenormals;
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
-
+    
     // In case we have more outputs than inputs, this code clears any output
     // channels that didn't contain input data, (because these aren't
     // guaranteed to be empty - they may contain garbage).
@@ -143,10 +143,59 @@ void Project13AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, ju
     // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
-
-  
+    
+    auto newDSPOrder = DSP_Order();
+    
+    //try to pull
+    while( dspOrderFifo.pull(newDSPOrder) )
+    {
+        
+    }
+    
+    //if you pulled, replace dspOrder;
+    if( newDSPOrder != DSP_Order() )
+        dspOrder = newDSPOrder;
+    
+    //now convert dspOrder into an array of pointers.
+    DSP_Pointers dspPointers;
+    
+    for( size_t i = 0; i < dspPointers.size(); ++i )
+    {
+        switch (dspOrder[i])
+        {
+            case DSP_Option::Phase:
+                dspPointers[i] = &phaser;
+                break;
+            case DSP_Option::Chorus:
+                dspPointers[i] = &chorus;
+                break;
+            case DSP_Option::Overdrive:
+                dspPointers[i] = &overdrive;
+                break;
+            case DSP_Option::LadderFilter:
+                dspPointers[i] = &ladderFilter;
+                break;
+            case DSP_Option::END_OF_LIST:
+                jassertfalse;
+                break;
+        }
+    }
+    
+    //now process:
+    auto block = juce::dsp::AudioBlock<float>(buffer);
+    auto context = juce::dsp::ProcessContextReplacing<float>(block);
+    
+    for( size_t i = 0; i < dspPointers.size(); ++i )
+    {
+        if( dspPointers[i] != nullptr )
+        {
+            dspPointers[i]->process(context);
+        }
+    }
+    
+    
+    
 }
-
 //==============================================================================
 bool Project13AudioProcessor::hasEditor() const
 {
