c98b32c38fdff5374a4ac2560f73c803e2631ec5
Kingbnjmn <business@kingbnjmn.com>
2025-08-14 21:11:29 -0500

    cleaned up selected tab switching, added comparator struct

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -165,6 +165,33 @@ void ExtendedTabbedButtonBar::itemDragEnter(const SourceDetails &dragSourceDetai
     juce::DragAndDropTarget::itemDragEnter(dragSourceDetails);
 }
 
+struct Comparator
+{
+    /*
+     This will use a comparator object to sort the elements into order.
+     The object passed must have a method of the form:
+     @code
+     int compare Elements (ElementType first, ElementType second);
+     @endcode
+     
+     ..and this method must return:
+     - a value of < 0 if the first comes before the second
+     - a value of 0 if the two objects are equivalent
+     - a value of > 0 if the second comes before the first
+     
+     To improve performance, the compareElements() method can be declared as static or const.
+     */
+    static int compareElements( juce::TabBarButton* first, juce::TabBarButton* second )
+    {
+        if( first->getX() < second->getX() )
+            return -1;
+        if( first->getX() == second->getX() )
+            return 0;
+        
+        return 1;
+    }
+};
+
 juce::Array<juce::TabBarButton*> ExtendedTabbedButtonBar::getTabs()
 {
     auto numTabs = getNumTabs();
@@ -174,6 +201,14 @@ juce::Array<juce::TabBarButton*> ExtendedTabbedButtonBar::getTabs()
     {
         tabs.getReference(i) = getTabButton(i);
     }
+    
+    auto unsorted = tabs;
+    Comparator comparator;
+    tabs.sort(comparator);
+    
+//    if( tabs != unsorted )
+//        jassertfalse;
+    
     return tabs;
 }
 
@@ -314,6 +349,13 @@ void ExtendedTabbedButtonBar::mouseDown(const juce::MouseEvent& e)
     DBG( "ExtendedTabbedButtonBar::mouseDown");
     if(auto tabButtonBeingDragged = dynamic_cast<ExtendedTabBarButton*>( e.originalComponent) )
     {
+        auto tabs = getTabs();
+        auto idx = tabs.indexOf(tabButtonBeingDragged);
+        if( idx != -1)
+        {
+            setCurrentTabIndex(idx);
+        }
+        
         startDragging(tabButtonBeingDragged->TabBarButton::getTitle(),
                       tabButtonBeingDragged,
                       dragImage);
